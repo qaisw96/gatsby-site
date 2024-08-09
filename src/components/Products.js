277304e0ff32c1a9d284632abdd915f3b1ac2307
qaisw96 <qaisw14@gmail.com>
2024-08-09 23:44:57 +0300

    increase cart

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion } from "framer-motion"
 import {
   addToCart,
   getCart,
@@ -49,79 +49,81 @@ const Products = ({ products }) => {
         <Alert message={alertMessage} onClose={handleAlertClose} />
       )}
       <div className="container grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 py-20">
-        {[...products].map(({ node }) => (
-          <motion.div
-            key={node.id}
-            className="bg-white rounded-md"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5 }}
-            onMouseEnter={() => setHoveredProduct(node.id)}
-            onMouseLeave={() => setHoveredProduct(null)}
-          >
-            {node.images && (
-              <Link to={`/products/${node.id}`}>
-                <GatsbyImage
-                  image={getImage(node.images[0])}
-                  alt={node.title}
-                  className="w-full"
-                />
-              </Link>
-            )}
-            <div className="p-4">
-              <h2 className="">{node.title}</h2>
-              <p className="text-sm text-primary mt-2 font-bold">
-                {node.price} دينار
-              </p>
-              <div className="text-center relative">
-                <button
-                  className={`text-sm rounded-sm mt-2 py-3 w-full ${
-                    isProductInCart(node.id)
-                      ? "bg-primary text-white"
-                      : "bg-secondary"
-                  }`}
-                  onClick={() =>
-                    isProductInCart(node.id)
-                      ? handleRemove(node.id)
-                      : handleAddToCart(node)
-                  }
-                >
-                  <span>
-                    {isProductInCart(node.id)
-                      ? "حذف من المشتريات"
-                      : "أضف للمشتريات"}
-                  </span>
+        {[...products].map(({ node }) => {
+          const isInCart = isProductInCart(node.id)
+          const quantity = getProductFromCart(node.id)?.quantity || 0
 
-                  {getProductFromCart(node.id)?.quantity && (
-                    <span className="mr-2 underline">
-                      {getProductFromCart(node.id)?.quantity}
-                    </span>
+          return (
+            <motion.div
+              key={node.id}
+              className="bg-white rounded-md"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              onMouseEnter={() => setHoveredProduct(node.id)}
+              onMouseLeave={() => setHoveredProduct(null)}
+            >
+              {node.images && (
+                <Link to={`/products/${node.id}`}>
+                  <GatsbyImage
+                    image={getImage(node.images[0])}
+                    alt={node.title}
+                    className="w-full"
+                  />
+                </Link>
+              )}
+              <div className="p-4">
+                <h2 className="">{node.title}</h2>
+                <p className="text-sm text-primary mt-2 font-bold">
+                  {node.price} دينار
+                </p>
+                <div className="text-center relative">
+                  <button
+                    className={`text-sm rounded-sm mt-2 py-3 w-full ${
+                      isInCart ? "bg-primary text-white" : "bg-secondary"
+                    }`}
+                    onClick={() =>
+                      isInCart ? handleRemove(node.id) : handleAddToCart(node)
+                    }
+                  >
+                    <span>{isInCart ? "تمت اضافته" : "أضف للمشتريات"}</span>
+
+                    {quantity > 0 && (
+                      <span className="mr-2 underline">{quantity}</span>
+                    )}
+                  </button>
+
+                  {isInCart && (
+                    <motion.div
+                      className={`absolute top-0 left-0 right-0 h-full flex justify-between items-center px-2 ${
+                        hoveredProduct === node.id || window.innerWidth < 768
+                          ? "opacity-100 pointer-events-auto"
+                          : "opacity-0 pointer-events-none"
+                      } transition-opacity duration-300`}
+                    >
+                      <motion.button
+                        className="text-white bg-slate-400 px-2 rounded-sm h-fit mt-2"
+                        initial={{ x: window.innerWidth < 768 ? 0 : "100%" }}
+                        animate={{ x: 0 }}
+                        onClick={() => handleQuantityChange(node.id, 1)}
+                      >
+                        +
+                      </motion.button>
+                      <motion.button
+                        className="text-white bg-slate-400 px-2 rounded-sm h-fit mt-2"
+                        initial={{ x: window.innerWidth < 768 ? 0 : "-100%" }}
+                        animate={{ x: 0 }}
+                        onClick={() => handleQuantityChange(node.id, -1)}
+                      >
+                        -
+                      </motion.button>
+                    </motion.div>
                   )}
-                </button>
-                {isProductInCart(node.id) && hoveredProduct === node.id && (
-                  <div className="absolute top-0 left-0 right-0 h-full flex justify-between items-center px-2">
-                    <motion.button
-                      className="text-white bg-slate-400 px-2 rounded-sm h-fit mt-2"
-                      initial={{ x: "-100%" }}
-                      animate={{ x: 0 }}
-                      onClick={() => handleQuantityChange(node.id, 1)}
-                    >
-                      +
-                    </motion.button>
-                    <motion.button
-                      className="text-white bg-slate-400 px-2 rounded-sm h-fit mt-2"
-                      initial={{ x: "100%" }}
-                      animate={{ x: 0 }}
-                      onClick={() => handleQuantityChange(node.id, -1)}
-                    >
-                      -
-                    </motion.button>
-                  </div>
-                )}
+                </div>
               </div>
-            </div>
-          </motion.div>
-        ))}
+            </motion.div>
+          )
+        })}
       </div>
     </div>
   )
